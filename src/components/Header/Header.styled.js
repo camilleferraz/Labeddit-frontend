406083a8bd1650bc80e836477f7f1447c9ba7641
camilleferraz <milleagf@gmail.com>
2023-04-25 18:11:17 -0300

    Finalizando

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderStyle = styled.header`
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background: #EDEDED;
+
+    div{
+        width: 33.33%;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    `   
---
+++ src/router/coordinator.js
@@ -0,0 +1,15 @@
+export const goToLoginPage = (navigate)=>{
+    navigate("/")
+}
+
+export const goToSignUpPage = (navigate)=>{
+    navigate("/signup")
+}
+
+export const goToHomePage = (navigate)=>{
+    navigate("/homepage")
+}
+
+export const goToPostPage = (navigate, post)=>{
+    navigate(`/posts/${post}`)
+}
